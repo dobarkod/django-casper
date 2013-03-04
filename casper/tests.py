5873afb2cfa95d1067f670acd5a0b1544fdcc2ca
Senko Rasic <senko.rasic@dobarkod.hr>
2013-03-04 12:00:05 +0100

    CasperTestCase: optionally use PhantomJS on-disk cache

--- casper/tests.py
+++ casper/tests.py
@@ -3,11 +3,29 @@ from subprocess import Popen, PIPE
 import os.path
 import sys
 
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.contrib.staticfiles.views import serve
+from django.utils.http import http_date
+
 __all__ = ['CasperTestCase']
+
+
+def staticfiles_handler_serve(self, request):
+    resp = serve(request, self.file_path(request.path), insecure=True)
+    if resp.status_code == 200:
+        resp["Expires"] = http_date(time.time() + 24 * 3600)
+    return resp
 
 
 class CasperTestCase(LiveServerTestCase):
     """LiveServerTestCase subclass that can invoke CasperJS tests."""
+
+    use_phantom_disk_cache = False
+
+    def __init__(self, *args, **kwargs):
+        super(CasperTestCase, self).__init__(*args, **kwargs)
+        if self.use_phantom_disk_cache:
+            StaticFilesHandler.serve = staticfiles_handler_serve
 
     def casper(self, test_filename, **kwargs):
         """CasperJS test invoker.
@@ -24,7 +42,7 @@ class CasperTestCase(LiveServerTestCase):
 
         kwargs.update({
             'load-images': 'no',
-            'disk-cache': 'no',
+            'disk-cache': 'yes' if self.use_phantom_disk_cache else 'no',
             'ignore-ssl-errors': 'yes',
             'url-base': self.live_server_url
         })
