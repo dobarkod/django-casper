8eaf1a70c6c5d78118d3f98aa77de2bfc8e00433
Nik Nyby <nikolas@gnu.org>
2015-12-07 18:54:25 -0500

    release 0.0.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ class CoverageCommand(BaseCommand):
 
 setup(
     name='django-casper',
-    version='0.0.3',
+    version='0.0.4',
     author='Senko Rasic',
     author_email='senko.rasic@goodcode.io',
     description='CasperJS test integration for Django',
