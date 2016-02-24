237fdbd91c125a1525e9f970ad5ebadb6188583d
Nik Nyby <nnyby@columbia.edu>
2016-02-24 14:58:04 -0500

    jscs fixes

--- casper/jslib/djangocasper.js
+++ casper/jslib/djangocasper.js
@@ -1,16 +1,17 @@
 /* global casper: true, phantom: true */
 
 module.exports = (function() {
-    var first_scenario = true;
+    var firstScenario = true;
 
     function opt(name, dfl) {
-        if (casper.cli.options.hasOwnProperty(name))
+        if (casper.cli.options.hasOwnProperty(name)) {
             return casper.cli.options[name];
-        else
+        } else {
             return dfl;
+        }
     }
 
-    function inject_cookies() {
+    function injectCookies() {
         var m = opt('url-base').match(/https?:\/\/([^:]+)(:\d+)?\//);
         var domain = m ? m[1] : 'localhost';
 
@@ -27,24 +28,27 @@ module.exports = (function() {
     }
 
     function scenario() {
-        var base_url = opt('url-base');
-        var start_url = base_url + arguments[0];
+        var baseUrl = opt('url-base');
+        var startUrl = baseUrl + arguments[0];
         var i;
 
-        casper.options.viewportSize = { width: 1024, height: 768 };
+        casper.options.viewportSize = {
+            width: 1024,
+            height: 768
+        };
 
-        if (first_scenario) {
-            inject_cookies();
+        if (firstScenario) {
+            injectCookies();
 
             casper.options.timeout = 60000;
             casper.options.onTimeout = function() {
-                casper.die("Timed out after 60 seconds.", 1);
+                casper.die('Timed out after 60 seconds.', 1);
             };
 
-            casper.start(start_url, arguments[1]);
-            first_scenario = false;
+            casper.start(startUrl, arguments[1]);
+            firstScenario = false;
         } else {
-            casper.thenOpen(start_url, arguments[1]);
+            casper.thenOpen(startUrl, arguments[1]);
         }
 
         for (i = 2; i < arguments.length; i++) {
@@ -56,12 +60,13 @@ module.exports = (function() {
         casper.run(function() { this.test.done(); });
     }
 
-    function assertAbsUrl(rel_url, str_msg) {
-        var regex_str = '^' + casper.cli.options['url-base'] + rel_url.replace(/\?/g, '\\?') + '$';
-        casper.test.assertUrlMatch(new RegExp(regex_str), str_msg);
+    function assertAbsUrl(relUrl, strMsg) {
+        var regexStr = '^' + casper.cli.options['url-base'] +
+            relUrl.replace(/\?/g, '\\?') + '$';
+        casper.test.assertUrlMatch(new RegExp(regexStr), strMsg);
     }
 
-    function qunit (url) {
+    function qunit(url) {
         scenario(url, function() {
             casper.waitFor(function() {
                 return casper.evaluate(function() {
@@ -69,14 +74,15 @@ module.exports = (function() {
                     return el && el.innerText.match('completed');
                 });
             }, function() {
-                casper.echo("Test output: " + casper.evaluate(function(){
-                    return document.getElementById('qunit-testresult').innerText;
+                casper.echo('Test output: ' + casper.evaluate(function() {
+                    return document.getElementById('qunit-testresult')
+                        .innerText;
                 }), 'INFO');
                 casper.test.assertEquals(
-                    casper.evaluate(function(){
+                    casper.evaluate(function() {
                         return document.getElementById('qunit-testresult')
                             .getElementsByClassName('failed')[0].innerText;
-                    }), "0");
+                    }), '0');
             });
         });
     }
