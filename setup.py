6ec1cee12ac54ac435a4ff5d386453fff34ada43
Nik Nyby <nnyby@columbia.edu>
2016-02-24 13:23:03 -0500

    update required django version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     packages=find_packages(),
-    install_requires=['Django>=1.6'],
+    install_requires=['Django>=1.7'],
     test_requires=['coverage'],
     cmdclass={
         'test': TestCommand,
